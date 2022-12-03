d9aa25f7bfc3bbe54e0f4ac203c3c03fdea2f852
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2022-12-03 19:49:55 +0530

    Add files via upload
    added all the features

--- etch-a-sketch/etch-a-sketch/main.js
+++ etch-a-sketch/etch-a-sketch/main.js
@@ -1,19 +1,67 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('hi');
-    Createboard(16, 16);
+    // console.log('hi');
+    Createboard(16);
+
+    let popup = document.querySelector("#popup");
+    popup.addEventListener("click", () => {
+        let size = Check();
+        Createboard(size);
+    })
 })
 
-function Createboard(rows, cols) {
+function Createboard(size) {
     let board = document.querySelector("#container");
 
-    board.style.gridTemplateColumns = `repeat(${rows},1fr)`
-    board.style.gridTemplateRows = `repeat(${cols},1fr)`
+    board.style.gridTemplateColumns = `repeat(${size},1fr)`
+    board.style.gridTemplateRows = `repeat(${size},1fr)`
 
-    let numDivs = rows *cols;
+    let numDivs = size * size;
 
-    for(let i = 0 ; i<numDivs; i++){
+    for (let i = 0; i < numDivs; i++) {
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
-        board.insertAdjacentElement("beforeend",div);
+        // div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover", colorDiv)
+        board.insertAdjacentElement("beforeend", div);
     }
 }
+
+function Check() {
+    let input = prompt("What will be the size of the board?")
+    let message = document.querySelector("#message")
+
+    if (input == "") {
+        message.innerHTML = "please enter a size";
+    }
+
+    else if (input < 0 || input > 100) {
+        message.innerHTML = "please select a number between 1 and 100"
+    }
+
+    else {
+        message.innerHTML = "now you can play!"
+    }
+    return input;
+}
+
+function colorDiv() {
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360},100% , 50%)`; 
+    }
+    else{
+        this.style.backgroundColor="black";
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div");
+    // let div = document.querySelector("#container");
+    divs.forEach((div)=>div.style.backgroundColor = "white");
+    // div.style.backgroundColor = "white";
+}   
