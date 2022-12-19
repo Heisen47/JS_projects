f48a17acfda2cfb756ceb7df45a70f6c161f563a
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2022-12-19 16:02:21 +0530

    Add files via upload

---
+++ calculator-TOP/script.js
@@ -0,0 +1,27 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText.replace(/[^-+/*\d]/g, ''));
+                } catch {
+                    display.prompt("Please enter a number then use the operator! Error");
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
